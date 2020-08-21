4a269a919a5c5b3ece9d1b75401f04b119cb758b
stephdraggo <61036246+stephdraggo@users.noreply.github.com>
2020-08-21 11:57:31 +1000

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int area;
+    public int currentInt = 0, highestInt = 0;
+    void Start()
+    {
+        //Area(15, 25);
+
+        HighestRandomNumber(10, 100);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Area(int l, int w)
+    {
+        area = l * w;
+        Debug.Log(area);
+    }
+
+    public void HighestRandomNumber(int amount, int range)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            currentInt = Random.Range(0, range + 1);
+            Debug.Log(currentInt);
+            if (currentInt > highestInt)
+            {
+                highestInt = currentInt;
+            }
+        }
+        Debug.Log("Highest Value: " + highestInt);
+    }
+}
---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    public Vector3 cubePosition;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (cubePosition.x < 5)
+        {
+            cubePosition = transform.position;
+            cubePosition.x += 10 * Time.deltaTime;
+            transform.position = cubePosition;
+        }
+    }
+}
